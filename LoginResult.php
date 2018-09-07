16cfcf99e2d347143f7377186b8765795fddff77
Roland Steinebrunner <steinebrunner.r@gmail.com>
2018-09-07 14:56:13 -0700

    file move

---
+++ LoginResult.php
@@ -0,0 +1,22 @@
+<!--
+Taco Blogs V 2.0
+Login Result Script V 1.0
+Programmers Roland, Kevin, Josh, Chuong
+9/7/2018
+Description:
+     Calls this to get login info from the form, then calls the script to check the database and outputs the appropriate php file
+Resources: PHP and MySQL web Development, www.w3schools.com
+-->
+<?php
+
+require_once("Service.php");
+
+$uname = $_GET['uname'];
+$pword = $_GET['pword'];
+$user = new Service();
+if($user->login($uname, $pword)){
+    include("LoginSuccess.php");
+}
+else{
+    include ("LoginError.php");
+}
---
+++ RegisterSuccess.php
@@ -0,0 +1,16 @@
+<!--
+Taco Blogs V 2.0
+Register Success V 1.0
+Programmers Roland, Kevin, Josh, Chuong
+9/7/2018
+Description:
+    Anouncemnet stating registration sucessfull
+Resources: PHP and MySQL web Development, www.w3schools.com
+-->
+<?php
+echo 'Registeration Successful';
+?>
+<html>
+Login
+<a href="Login.html">Login</a>
+</html>
