5823113b0e8317ec5c8eca53efbd4d1ccadb14ae
Roland Steinebrunner <steinebrunner.r@gmail.com>
2018-09-06 17:43:39 -0700

    TEXT field added

--- index.php
+++ index.php
@@ -3,3 +3,29 @@
 	echo date('m/d/Y h:i:s a');
 	echo "Kevin added a date and is showing that I can push to Heroku";
 ?>
+	<form role="form" method="post" action="" autocomplete="off">
+
+                <div class="form-group">
+                    <input type="text" name="username" id="username" class="form-control input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo $_POST['username']; } ?>" tabindex="1">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value="<?php if(isset($error)){ echo $_POST['email']; } ?>" tabindex="2">
+                </div>
+                <div class="row">
+                    <div class="col-xs-6 col-sm-6 col-md-6">
+                        <div class="form-group">
+                            <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
+                        </div>
+                    </div>
+                    <div class="col-xs-6 col-sm-6 col-md-6">
+                        <div class="form-group">
+                            <input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xs-6 col-md-6"><input type="submit" name="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="5"></div>
+                </div>
+            </form>
+
