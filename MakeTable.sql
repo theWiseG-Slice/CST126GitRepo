e45a1059b7b56e9e9331c89f8011b25aaf983531
RSteinebrunner <43014086+RSteinebrunner@users.noreply.github.com>
2018-09-07 15:43:39 -0600

    Add files via upload

---
+++ MakeTable.sql
@@ -0,0 +1,8 @@
+USE try1;
+create table users(
+   username varchar(50) NOT NULL,
+   pword VARCHAR(100) NOT NULL,
+   firstname VARCHAR(50) NOT NULL,
+   lastname varchar(50),
+   email varchar(100)
+);
