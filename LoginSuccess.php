554419286fa3a117f071a4365cd251c3496465a8
RSteinebrunner <43014086+RSteinebrunner@users.noreply.github.com>
2018-09-07 14:04:02 -0700

    Update LoginSuccess.php

--- LoginSuccess.php
+++ LoginSuccess.php
@@ -1,10 +1,13 @@
+<!--
+Taco Blogs V 2.0
+Login Success Script V 1.0
+Programmers Roland, Kevin, Josh, Chuong
+9/7/2018
+Description:
+      After Checking login anounces a sucesful login
+Resources: PHP and MySQL web Development, www.w3schools.com
+-->
 <?php
-/**
- * Created by PhpStorm.
- * User: Alyssa
- * Date: 9/7/2018
- * Time: 2:26 AM
- */
 echo "Login Success";
 ?>
 <html>
