bf7a59b10fe68ebe8596629a77a11d14bf45eb8c
RSteinebrunner <43014086+RSteinebrunner@users.noreply.github.com>
2018-09-07 14:10:25 -0700

    Update RegisterError.php

--- RegisterError.php
+++ RegisterError.php
@@ -1,10 +1,12 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alyssa
- * Date: 9/7/2018
- * Time: 9:55 AM
- */
+<!--
+Taco Blogs V 2.0
+Login Page V 1.0
+Programmers Roland, Kevin, Josh, Chuong
+9/7/2018
+Description:
+     Anouncment stating that the registration resulted in an error
+Resources: PHP and MySQL web Development, www.w3schools.com
+--><?php
 echo 'register error';
 ?>
 <html>
