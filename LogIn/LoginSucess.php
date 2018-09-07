14568cb5e6db39ddcceb800379f8a00bba2a253e
RSteinebrunner <43014086+RSteinebrunner@users.noreply.github.com>
2018-09-07 14:40:12 -0700

    Create LoginSucess.php

---
+++ LogIn/LoginSucess.php
@@ -0,0 +1,16 @@
+<!--
+Taco Blogs V 2.0
+Login Success Script V 1.0
+Programmers Roland, Kevin, Josh, Chuong
+9/7/2018
+Description:
+      After Checking login anounces a sucesful login
+Resources: PHP and MySQL web Development, www.w3schools.com
+-->
+<?php
+echo "Login Success";
+?>
+<html>
+Go back
+<a href="Login.html">Login</a>
+</html>
