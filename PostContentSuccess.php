7235bbf56b34068e472a683a1ece5b8700d48065
Roland Steinebrunner <steinebrunner.r@gmail.com>
2018-10-09 18:10:24 -0700

    wrapper

--- PostContentSuccess.php
+++ PostContentSuccess.php
@@ -21,7 +21,7 @@ Resources: PHP and MySQL web Development
     <title>Taco's Blog</title>
     <link type="text/css" rel="stylesheet" href="style.css"/>
 </head>
-<div <!--id="wrapper"--> >
+<div id="wrapper" >
     <div <!--id="header"--> >
         <div <!--id="post"--> >
             <h2>Taco's Blog Page</h2>
