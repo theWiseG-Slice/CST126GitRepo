93e6e8ca67846498b41f553a6acdaca392125f0c
TheSmartDeveloper <kgsmart95@gmail.com>
2018-10-15 17:43:00 -0700

    post content page style work

--- PostContentSuccess.php
+++ PostContentSuccess.php
@@ -10,8 +10,8 @@ Resources: PHP and MySQL web Development
 ?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script>-->
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>-->
+<script>
     $(document).ready(function () {
         $("[data-toggle=tooltip]").tooltip();
     });
