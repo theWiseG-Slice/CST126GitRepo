73784242b8ac4187fdecf3240676a77fd02e2cfa
TheSmartDeveloper <kgsmart95@gmail.com>
2018-10-15 17:40:37 -0700

    post content page style work

--- PostContentSuccess.php
+++ PostContentSuccess.php
@@ -6,12 +6,12 @@ Programmers Roland, Kevin, Josh, Chuong
 Description:
      display all posts in html
 Resources: PHP and MySQL web Development
---><?php
+<?php
 ?>
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<script>
+<script>-->
     $(document).ready(function () {
         $("[data-toggle=tooltip]").tooltip();
     });
