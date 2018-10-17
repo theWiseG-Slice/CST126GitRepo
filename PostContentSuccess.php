37fef89a9619df989835c25d0eb46c65472a0e6d
TheSmartDeveloper <kgsmart95@gmail.com>
2018-10-16 20:25:04 -0700

    previous post format fix

--- PostContentSuccess.php
+++ PostContentSuccess.php
@@ -82,6 +82,8 @@ $result = mysqli_query($connection, "SELECT * from content ORDER BY postnumber D
 						<?php echo "Post by: " . $row['username']; ?>
 						|
 						<?php echo "Date: ".$row['datetime']; ?>
+						<div class="bar">
+						</div>
 					</div>
 	            </div>
     	    </div>
