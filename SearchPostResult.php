1ef1cc30dbdde13c4d343bd1443f077efc8423b3
chuongngd <chuongngd@gmail.com>
2018-10-21 01:44:12 -0400

    Add files via upload

---
+++ SearchPostResult.php
@@ -0,0 +1,54 @@
+
+<h3>Search Result</h3>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chuong
+ * Date: 10/3/2018
+ * Time: 1:57 PM
+ */
+$username = $_GET['uname1'];
+$tagName = $_GET['tag'];
+$_SESSION['uname'] = $username;
+$host = 'localhost';
+$username = 'root';
+$password = 'root';
+$database = 'myblog';
+$connection = mysqli_connect($host, $username, $password, $database, 8889);
+$result = mysqli_query($connection, "SELECT tagID FROM tags WHERE tagName='$tagName'");
+if ($result->num_rows > 0) {
+    $tagID = mysqli_fetch_array($result)['tagID'];
+    $result1 = mysqli_query($connection, "SELECT contentID FROM tags_content WHERE tagID='$tagID'");
+    if ($result1->num_rows > 0) {
+        while ($row1 = mysqli_fetch_array($result1)) {
+            $contentID = $row1['contentID'];
+            $result3 = mysqli_query($connection, "SELECT * FROM content WHERE contentID='$contentID'");
+            while ($row3 = $result3->fetch_assoc()) {
+                ?>
+                <div style="background-color:lightblue">
+                    <?php
+                    echo "" . $row3['title'];
+                    echo "</br>";
+                    echo "" . $row3['content'];
+                    echo "</br>";
+                    echo "" . $row3['author'];
+                    ?>
+                    </div>
+                <?php
+
+                echo "</br>";
+                echo "</br>";
+                echo "</br>";
+
+                    ?>
+
+                <?php
+            }
+        }
+    }
+} else {
+    echo "No Search match with this tag";
+}
+?>
+
+<a href="PostContentSuccess.php">Go back</a>
