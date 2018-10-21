6fe01646fa3f43b213d03b50748e1459c499474e
chuongngd <chuongngd@gmail.com>
2018-10-21 01:42:36 -0400

    new file

---
+++ AddTagResult.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chuong
+ * Date: 10/3/2018
+ * Time: 1:57 PM
+ */
+require_once("Service.php");
+$username_tag = $_GET['username_tag'];
+$username_ses = $_GET['username_session'];
+$contentID = $_GET['contentID_tag'];
+$tagName = $_GET['tagName'];
+$_SESSION['uname'] = $username_ses;
+$user = new Service();
+if($username_ses == '')
+{
+    include ("Login.html");
+}
+else {
+    if($username_ses != $username_tag )
+    {
+        include ("AddTagError.html");
+    }
+    else {
+        if ($user->addTag($contentID,$tagName) == true) {
+
+            include("PostContentSuccess.php");
+        } else {
+            include("Login.html");
+        }
+    }
+}
