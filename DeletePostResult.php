022bd72e37e189ed4cd103fd4b182a8df01c8205
chuongngd <chuongngd@gmail.com>
2018-10-21 01:43:29 -0400

    new files

---
+++ DeletePostResult.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chuong
+ * Date: 10/3/2018
+ * Time: 1:57 PM
+ */
+require_once("Service.php");
+$username_del = $_GET['uname_del'];
+$username_ses = $_GET['uname_ses'];
+$content = $_GET['content_del'];
+$title = $_GET['title_del'];
+$contentID=$_GET['contentID_del'];
+$_SESSION['uname'] = $username_ses;
+
+$user = new Service();
+if($username_ses == '')
+{
+    include ("DeleteError.html");
+}
+else {
+    if($username_ses != $username_del )
+    {
+        include ("DeleteError.html");
+    }
+    else {
+        if ($user->deletePost($contentID) == true) {
+
+            include("PostContentSuccess.php");
+        } else {
+
+            include("DeleteError.html");
+        }
+    }
+}
