ca080a5a7067998f484aa43a05a6d552648bad6a
TheSmartDeveloper <kgsmart95@gmail.com>
2018-11-08 14:12:33 -0700

    test

--- PostContentSuccess.php
+++ PostContentSuccess.php
@@ -1,9 +1,9 @@
 <?php
 
 ?>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<!--<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>-->
 <script>
     $(document).ready(function () {
         $("[data-toggle=tooltip]").tooltip();
