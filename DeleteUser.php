08f7eefacbf6d84b87d40460752818025d0c24bf
TheSmartDeveloper <kgsmart95@gmail.com>
2019-01-31 15:52:42 -0700

    clean of everything

--- DeleteUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("Service.php");
-$username_del = $_GET['user_delete'];
-$username_ses = $_GET['uname_ses'];
-$_SESSION['uname'] = $username_ses;
-
-$user = new Service();
-if ($user->deleteUser($username_del))
-    include "showAdminPage.php";
